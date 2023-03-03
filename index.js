29472cb3050c37a4ecdae85a0c370ad600512c4c
Tino <tino5280@gmail.com>
2023-03-02 20:57:10 -0700

    fin

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki" ]
+let event = "suprise"
+function writeCards(names, event){
+let messages = [];
+for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+
+}
+return messages
+}
+
+function countDown(num) {
+    while (num > -1) {
+        console.log(num--)
+    }
+}
